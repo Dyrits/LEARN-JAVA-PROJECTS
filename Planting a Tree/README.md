6eeb76070049822f83e0e0d766df9f247bccf4a7
dyrits <dyrits@outlook.fr>
2020-05-28 11:25:43 +0200

    Initial commit.

---
+++ Planting a Tree/README.md
@@ -0,0 +1,24 @@
+# Planting a tree
+
+## More about this project
+
+### Related course
+[CODE]CADEMY PRO | LEARN JAVA: 01. Hello World  
+
+### Description of the project by [CODE]CADEMY PRO
+Introduce yourself to users and plant a tree for them!
+
+### Technologies | Libraries | Frameworks | Tools  
+- Java
+
+### Details | Comments
+- This project has been made from scratch following specific instructions. 
+
+### Status
+Completed 
+
+#### Last update
+28/05/2020
+
+#### Last update (README.md)
+28/05/2020
---
+++ Planting a Tree/src/Tree.java
@@ -0,0 +1,15 @@
+public class Tree {
+	public static void main(String[] args) {
+		// We’re going to introduce ourselves and print a tree to the screen.
+		System.out.println("Hey there, I'm Dylan and I’m learning to code in Java!");
+		System.out.println("I’m going to plant an ugly tree today!");
+		
+		System.out.println(); // Blank space.
+		
+	    System.out.println("  *  ");
+	    System.out.println(" *** ");
+	    System.out.println(" *** ");
+	    System.out.println("  *  ");
+	    System.out.println("  *  ");   
+	}
+}
