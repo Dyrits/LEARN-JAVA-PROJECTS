626e882cd45316d67846df166018610859ced7e1
dyrits <dyrits@outlook.fr>
2020-06-02 11:45:50 +0200

    'Continents and Cities' added.

---
+++ 04 - Conditionals and Control Flow/Continents and Cities/README.md
@@ -0,0 +1,30 @@
+# Continents and Cities
+
+## More about this project
+
+### Related course
+[CODE]CADEMY PRO | LEARN JAVA: 04 Conditionals and Control Flow
+
+### Description of the project by [CODE]CADEMY PRO
+Planet Earth is a magical place. Let’s practice the Java switch statement that you learned about.
+
+Write a **Continents.java** program that will print out a continent and the largest city in that continent, based on the value of an integer.
+
+The instructions provided are general guidelines. Upon completion of the project, feel free to add functionality of your own
+
+
+### Technologies | Libraries | Frameworks | Tools  
+- Java
+
+### Details | Comments
+- This project has been made from scratch following specific instructions. 
+
+### Status
+Completed 
+
+#### Last update
+02/06/2020
+
+#### Last update (README.md)
+02/06/2020
+ 
---
+++ 04 - Conditionals and Control Flow/Continents and Cities/src/Continents.java
@@ -0,0 +1,36 @@
+public class Continents {
+	/*
+	 This class will print out a continent and the largest city in that continent, based on the value of an integer.
+	 */
+	
+	public static void main(String[] args) {
+		int continent = 4;
+		
+		switch (continent) {
+		case 1:
+			System.out.println("North America: Mexico City, Mexico");
+			break;
+		case 2:
+			System.out.println("South America: Sao Paulo, Brazil");
+			break;
+		case 3:
+			System.out.println("Europe: Moscow, Russia");
+			break;
+		case 4:
+			System.out.println("Africa: Lagos, Nigeria");
+			break;
+		case 5:
+			System.out.println("Asia: Shanghai, China");
+			break;
+		case 6:
+			System.out.println("Australia: Sydney, Australia");
+			break;
+		case 7:
+			System.out.println("Antarctica: McMurdo Station, US");
+			break;
+		default:
+			System.out.println("Undefined continent!");
+			break;			
+		}
+	}
+}
